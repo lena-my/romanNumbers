19379537a46072045431b19501a8d00149524825
Helena Miura <helenamiura@gmail.com>
2024-06-12 16:33:11 +0200

    added roman number pattern to test

--- romanNumbers/Program.cs
+++ romanNumbers/Program.cs
@@ -3,12 +3,14 @@
 
 
 
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+
 while (true)
 {
     Console.WriteLine("Enter a roman number: ");
     string romanNumber = Console.ReadLine();
     int result = 0;
-   
 
     Dictionary<string, int> numbers = new Dictionary<string, int>();
 
@@ -21,24 +23,32 @@ while (true)
     numbers.Add("M", 1000);
 
     int previousNumber = 0;
+    string romanNumberPattern = @"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"; // regex to roman numbers
 
     for (int i = romanNumber.Length - 1; i >= 0; i--) // loop into entered romanNumber
     {
         int currentNumber = numbers[$"{romanNumber[i]}"]; // current roman number in the loop
+        bool isARomanNumber = Regex.IsMatch(romanNumber, romanNumberPattern); // pattern that defines roman number
 
-        if (currentNumber >= previousNumber) {
-            result += currentNumber; // ex. VI = 5 + 1 = 6
-        }
-        else {
-            result -=  currentNumber; // 
-        }
+        if (isARomanNumber == true)
+        { 
+            if (currentNumber >= previousNumber)
+            {
+                result += currentNumber; // ex. VI = 5 + 1 = 6
+            }
+            else
+            {
+                result -= currentNumber; // 
+            }
 
-        previousNumber = currentNumber; // previous number un the loop
+            previousNumber = currentNumber; // previous number un the loop
+        }
+        else
+        { // if it is not a roman number
+            Console.WriteLine("Invalid number.");
+        }
     }
-
-
     Console.WriteLine("result = " + result);
-
 }
 
 
